e2df6dec464f0d9c8c11f159892ef58dd2b46be8
max_mu <max_mu@2af63af8-f945-0410-af61-887fe020137e>
2008-05-22 14:48:09 +0000

    Fix bug of cannot run SuperActionHero. getDisplayableHeight() return ScreenSkin.HEIGHT-TitleSkin.HEIGHT-SoftButtonSkin.HEIGHT if non-fullscreenmode and currentDisplay == null

--- trunk/midp/src/highlevelui/lcdlf/lfjava/classes/javax/microedition/lcdui/DisplayableLFImpl.java
+++ trunk/midp/src/highlevelui/lcdlf/lfjava/classes/javax/microedition/lcdui/DisplayableLFImpl.java
@@ -948,7 +948,7 @@ class DisplayableLFImpl implements DisplayableLF {
         if (!owner.isInFullScreenMode) {
             h = currentDisplay != null ?
             currentDisplay.getDisplayableHeight() :
-            ScreenSkin.HEIGHT;
+            ScreenSkin.HEIGHT-TitleSkin.HEIGHT-SoftButtonSkin.HEIGHT;
         } else {
             h = ScreenSkin.HEIGHT;
         }
