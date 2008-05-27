cd0a848f4bb6ecf94849e093031d665a56a40aaf
youyu45 <youyu45@2af63af8-f945-0410-af61-887fe020137e>
2008-05-27 05:00:31 +0000

    Fixed MIPS build: now it runs one assembly routine (primordial_to_current_thread) before crashing

--- trunk/cldc/build/linux_mips_qemu/rebuild_loop.sh
+++ trunk/cldc/build/linux_mips_qemu/rebuild_loop.sh
@@ -7,9 +7,16 @@ if test "$BUILD" = ""; then
     BUILD=product;
 fi
 
-make loopgen
+if test "$NUM_JOBS" = ""; then
+    NUM_JOBS=2
+fi
+
+# At this point, there's no point building product VM with optimization.
+export PRODUCT_DEBUG=true
+
+make parallel_loopgen NUM_JOBS=$NUM_JOBS || exit 1
 touch ${JVMBuildSpace}/linux_mips_qemu/target/${BUILD}/*.o
 rm -f ${JVMBuildSpace}/linux_mips_qemu/target/${BUILD}/Interpreter_mips.s
 rm -f ${JVMBuildSpace}/linux_mips_qemu/target/${BUILD}/Interpreter_mips.o
-make _${BUILD}
+time make _${BUILD}
 
