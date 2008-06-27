5255d3b2a8306bd3537d2fabaef01f696d0616fe
max_mu <max_mu@2af63af8-f945-0410-af61-887fe020137e>
2008-06-27 18:12:16 +0000

    Improve performance of FileConnection InputStream.skip()

--- jsr75/src/core/classes/com/sun/midp/io/j2me/file/Protocol.java
+++ jsr75/src/core/classes/com/sun/midp/io/j2me/file/Protocol.java
@@ -188,7 +188,9 @@ public class Protocol extends ConnectionBaseAdapter implements FileConnection {
 
         fileHandler.openForRead();
 
-        fis = super.openInputStream();
+        super.openInputStream();
+
+        fis = new FileConnectionInputStream(this);
 
         return fis;
     }
@@ -1440,3 +1442,369 @@ class EscapedUtil {
     }
 
 } // End  of EscapeUtil
+
+/**
+ * Input stream for the connection
+ */
+class FileConnectionInputStream extends InputStream {
+
+    /** Pointer to the connection. */
+    private Protocol parent;
+
+    /** Buffer for single char reads. */
+    byte[] buf = new byte[1];
+
+    /**
+      * Buffer for mark/reset funtionality support.
+      * <code>null</code> value indicates <code>mark</code> was not called or
+      * <code>readlimit</code> value of the last <code>mark</code> was exceeded.
+      */
+    byte[] markBuf = null;
+
+    /** The size of data stored in <code>markBuf</code>. */
+    int markSize = 0;
+
+    /** Current position in <code>markBuf</code> to read data from. */
+    int markPos = 0;
+
+    /**
+     * Indicates whether <code>reset</code> method was called.
+     * If so, data is read from <code>markBuf</code> buffer
+     * otherwise via <code>parent.readBytes</code> method.
+     */
+    boolean isReadFromBuffer = false;
+
+    /**
+     * Constructs a BaseInputStream for a ConnectionBaseAdapter.
+     *
+     * @param parent pointer to the connection object
+     *
+     * @exception  IOException  if an I/O error occurs.
+     */
+    FileConnectionInputStream(Protocol parent) throws IOException {
+        this.parent = parent;
+    }
+
+    /**
+     * Check the stream is open
+     *
+     * @exception  InterruptedIOException  if it is not.
+     */
+    private void ensureOpen() throws InterruptedIOException {
+        if (parent == null) {
+            throw new InterruptedIOException("Stream closed");
+        }
+    }
+
+    /**
+     * Returns the number of bytes that can be read (or skipped over) from
+     * this input stream without blocking by the next caller of a method for
+     * this input stream.  The next caller might be the same thread or
+     * another thread.
+     *
+     * <p>The <code>available</code> method always returns <code>0</code> if
+     * {@link ConnectionBaseAdapter#available()} is
+     * not overridden by the subclass.
+     *
+     * @return     the number of bytes that can be read from this input stream
+     *             without blocking.
+     * @exception  IOException  if an I/O error occurs.
+     */
+    public int available() throws IOException {
+
+        ensureOpen();
+
+        return parent.available();
+    }
+
+    /**
+     * Reads the next byte of data from the input stream. The value byte is
+     * returned as an <code>int</code> in the range <code>0</code> to
+     * <code>255</code>. If no byte is available because the end of the stream
+     * has been reached, the value <code>-1</code> is returned. This method
+     * blocks until input data is available, the end of the stream is detected,
+     * or an exception is thrown.
+     *
+     * @return     the next byte of data, or <code>-1</code> if the end of the
+     *             stream is reached.
+     * @exception  IOException  if an I/O error occurs.
+     */
+    public int read() throws IOException {
+        if (read(buf, 0, 1) > 0) {
+            return (buf[0] & 0xFF);
+        }
+
+        return -1;
+    }
+
+    /**
+     * Reads up to <code>len</code> bytes of data from the input stream into
+     * an array of bytes.  An attempt is made to read as many as
+     * <code>len</code> bytes, but a smaller number may be read, possibly
+     * zero. The number of bytes actually read is returned as an integer.
+     *
+     * <p> This method blocks until input data is available, end of file is
+     * detected, or an exception is thrown.
+     *
+     * <p> If <code>b</code> is <code>null</code>, a
+     * <code>NullPointerException</code> is thrown.
+     *
+     * <p> If <code>off</code> is negative, or <code>len</code> is negative, or
+     * <code>off+len</code> is greater than the length of the array
+     * <code>b</code>, then an <code>IndexOutOfBoundsException</code> is
+     * thrown.
+     *
+     * <p> If <code>len</code> is zero, then no bytes are read and
+     * <code>0</code> is returned; otherwise, there is an attempt to read at
+     * least one byte. If no byte is available because the stream is at end of
+     * file, the value <code>-1</code> is returned; otherwise, at least one
+     * byte is read and stored into <code>b</code>.
+     *
+     * <p> The first byte read is stored into element <code>b[off]</code>, the
+     * next one into <code>b[off+1]</code>, and so on. The number of bytes read
+     * is, at most, equal to <code>len</code>. Let <i>k</i> be the number of
+     * bytes actually read; these bytes will be stored in elements
+     * <code>b[off]</code> through <code>b[off+</code><i>k</i><code>-1]</code>,
+     * leaving elements <code>b[off+</code><i>k</i><code>]</code> through
+     * <code>b[off+len-1]</code> unaffected.
+     *
+     * <p> In every case, elements <code>b[0]</code> through
+     * <code>b[off]</code> and elements <code>b[off+len]</code> through
+     * <code>b[b.length-1]</code> are unaffected.
+     *
+     * <p> If the first byte cannot be read for any reason other than end of
+     * file, then an <code>IOException</code> is thrown. In particular, an
+     * <code>IOException</code> is thrown if the input stream has been closed.
+     *
+     * @param      b     the buffer into which the data is read.
+     * @param      off   the start offset in array <code>b</code>
+     *                   at which the data is written.
+     * @param      len   the maximum number of bytes to read.
+     * @return     the total number of bytes read into the buffer, or
+     *             <code>-1</code> if there is no more data because the end of
+     *             the stream has been reached.
+     * @exception  IOException  if an I/O error occurs.
+     * @see        java.io.InputStream#read()
+     */
+    public int read(byte b[], int off, int len) throws IOException {
+        int test;
+
+        ensureOpen();
+
+        if (len == 0) {
+            return 0;
+        }
+
+        /*
+         * test the parameters so the subclass will not have to.
+         * this will avoid crashes in the native code
+         */
+        test = b[off] + b[len - 1] + b[off + len - 1];
+
+        // use parent's mark/reset functionality
+        // if the parent supports the own one
+        if (parent.markSupported()) {
+            return parent.readBytes(b, off, len);
+        }
+
+        // read data from mark buffer if reset method was called
+        if (isReadFromBuffer) {
+            int dataSize = markSize - markPos;
+            if (dataSize > 0) {
+                int copySize = (dataSize > len) ? len : dataSize;
+                System.arraycopy(markBuf, markPos, b, off, copySize);
+                markPos += copySize;
+
+                // read data directly from the stream
+                // if size of data in the buffer is not enough
+                int readSize = 0;
+                if (copySize < len) {
+                    readSize = parent.readBytes(
+                        b, off + copySize, len - copySize);
+
+                    // check if eos is reached
+                    if (readSize == -1) {
+                        readSize = 0;
+                    } else {
+                        // check the mark buffer overflow
+                        if (markSize + readSize > markBuf.length) {
+                            markBuf = null;
+                        // cache the data in the mark buffer
+                        } else {
+                            System.arraycopy(
+                                b, off + copySize, markBuf, markSize, readSize);
+                            markSize += readSize;
+                        }
+                    }
+
+                    isReadFromBuffer = false;
+                }
+
+                return copySize + readSize;
+            } else {
+                isReadFromBuffer = false;
+            }
+        }
+
+        int readSize = parent.readBytes(b, off, len);
+
+        // fill mark buffer if exists
+        if (markBuf != null) {
+            if (readSize > 0) {
+                // check the mark buffer overflow
+                if (markSize + readSize > markBuf.length) {
+                    markBuf = null;
+                    // cache the data in the mark buffer
+                } else {
+                    System.arraycopy(b, off, markBuf, markSize, readSize);
+                    markSize += readSize;
+                }
+            }
+        }
+
+        return readSize;
+    }
+
+    /**
+     * Closes this input stream and releases any system resources associated
+     * with the stream.
+     *
+     * @exception  IOException  if an I/O error occurs.
+     */
+    public void close() throws IOException {
+        if (parent != null) {
+            parent.closeInputStream();
+            parent = null;
+        }
+	// free buffer used by mark/reset operations if it was allocated
+	markBuf = null;
+    }
+
+    /**
+     * Tests if this input stream supports the <code>mark</code> and
+     * <code>reset</code> methods.
+     *
+     * <p>The <code>markSupported</code> method of
+     * <code>BaseInputStream</code> returns <code>true</code>.
+     *
+     * @return  always <code>true</code>
+     *
+     * @see     BaseInputStream#mark(int)
+     * @see     BaseInputStream#reset()
+     * @see     java.io.InputStream#markSupported()
+     * @see     java.io.InputStream#mark(int)
+     * @see     java.io.InputStream#reset()
+     */
+    public boolean markSupported() {
+        return true;
+    }
+
+    /**
+     * Marks the current position in this input stream. A subsequent call to
+     * the <code>reset</code> method repositions this stream at the last marked
+     * position so that subsequent reads re-read the same bytes.
+     *
+     * <p> The <code>readlimit</code> arguments tells this input stream to
+     * allow that many bytes to be read before the mark position gets
+     * invalidated.
+     *
+     * <p> The stream remembers all the bytes read after the call to
+     * <code>mark</code> and stands ready to supply those same bytes again
+     * if and whenever the method <code>reset</code> is called.
+     *  However, the stream is not remember any data at all if more
+     * than <code>readlimit</code> bytes are read from the stream before
+     * <code>reset</code> is called.
+     *
+     * @param   readlimit   the maximum limit of bytes that can be read before
+     *                      the mark position becomes invalid.
+     * @see     BaseInputStream#reset()
+     * @see     java.io.InputStream#reset()
+     * @see     java.io.InputStream#mark(int)
+     */
+    public synchronized void mark(int readlimit) {
+        // check whether the stream is closed
+        if (parent == null) {
+            return;
+        }
+
+        // use parent's mark/reset functionality
+        // if the parent supports the own one
+        if (parent.markSupported()) {
+            parent.mark(readlimit);
+        } else {
+            byte[] oldBuf = markBuf;
+            markBuf = new byte[readlimit];
+
+            // copy relevant data from old buffer if any
+            if (isReadFromBuffer) {
+                int oldDataSize = markSize - markPos;
+                int copySize = (readlimit > oldDataSize) ?
+                    oldDataSize : readlimit;
+                System.arraycopy(oldBuf, markPos, markBuf, 0, copySize);
+                markSize = copySize;
+            } else {
+                markSize = 0;
+            }
+            markPos = 0;
+        }
+    }
+
+    /**
+     * Repositions this stream to the position at the time the
+     * <code>mark</code> method was last called on this input stream.
+     *
+     * <p> If the method <code>mark</code> has not been called since
+     * the stream was created, or the number of bytes read from the stream
+     * since <code>mark</code> was last called is larger than the argument
+     * to <code>mark</code> at that last call, then an
+     * <code>IOException</code> is thrown.
+     *
+     * <p> If such an <code>IOException</code> is not thrown, then the
+     * stream is reset to a state such that all the bytes read since the
+     * most recent call to <code>mark</code> will be resupplied
+     * to subsequent callers of the <code>read</code> method, followed by
+     * any bytes that otherwise would have been the next input data as of
+     * the time of the call to <code>reset</code>.
+     *
+     * @exception  IOException  if this stream has not been marked or if the
+     *                          mark has been invalidated;
+     *                          or if the stream is closed
+     * @see     BaseInputStream#mark(int)
+     * @see     java.io.InputStream#mark(int)
+     * @see     java.io.InputStream#reset(int)
+     * @see     java.io.IOException
+     */
+    public synchronized void reset() throws IOException {
+        ensureOpen();
+
+        // use parent's mark/reset functionality
+        // if the parent supports the own one
+        if (parent.markSupported()) {
+            parent.reset();
+        } else {
+            if (markBuf == null) {
+                throw new IOException("Invalid mark position");
+            }
+            markPos = 0;
+            isReadFromBuffer = true;
+        }
+    }
+
+    static private byte[] dummy_buffer = new byte[64*1024];
+    public long skip(long n) throws IOException
+    {
+        int readBytesTotal = 0;        
+        int readBytes = 0;
+        while (n > 0){     
+            readBytes = read( dummy_buffer, 0, (int)((n > 64*1024) ? 64*1024 : n) );
+            if ( readBytes <= 0 ) {
+                break;
+            } else {
+                readBytesTotal += readBytes;
+                n -= readBytes;
+            }
+        }
+        return (long)readBytesTotal;
+    }
+}
+
