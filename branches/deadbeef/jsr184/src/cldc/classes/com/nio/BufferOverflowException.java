e8462b829834b09e3ff5c8ff6b00e9434a8b94da
max_mu <max_mu@2af63af8-f945-0410-af61-887fe020137e>
2008-12-05 14:23:16 +0000

    removed

--- branches/deadbeef/jsr184/src/cldc/classes/com/nio/BufferOverflowException.java
+++
@@ -1,8 +0,0 @@
-package com.nio;
-
-public class BufferOverflowException extends RuntimeException {
-	private static final long serialVersionUID = -5484897634319144535L;
-
-	public BufferOverflowException() {
-	}
-}
