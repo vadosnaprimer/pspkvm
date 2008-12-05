ae43d969d26fd133cd4bc94d1478baf4c3613763
max_mu <max_mu@2af63af8-f945-0410-af61-887fe020137e>
2008-12-05 14:23:16 +0000

    removed

--- jsr184/src/cldc/classes/com/nio/BufferUnderflowException.java
+++
@@ -1,8 +0,0 @@
-package com.nio;
-
-public class BufferUnderflowException extends RuntimeException {
-	private static final long serialVersionUID = -1713313658691622206L;
-
-	public BufferUnderflowException() {
-	}
-}
