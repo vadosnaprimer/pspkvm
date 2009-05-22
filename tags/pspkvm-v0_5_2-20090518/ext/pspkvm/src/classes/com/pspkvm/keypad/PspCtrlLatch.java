c1af8103c80f1b8b633da8af3846278c4d602cff
max_mu <max_mu@2af63af8-f945-0410-af61-887fe020137e>
2009-05-22 18:46:53 +0000

    Removed file/folder

--- tags/pspkvm-v0_5_2-20090518/ext/pspkvm/src/classes/com/pspkvm/keypad/PspCtrlLatch.java
+++
@@ -1,9 +0,0 @@
-package com.pspkvm.keypad;
-
-public class PspCtrlLatch {
-    public int uiMake;
-    public int uiBreak;
-    public int uiPress;
-    public int uiRelease;
-}
-
--- tags/pspkvm-v0_5_2-20090518/javacall/implementation/win32/midp/misc.c
+++
@@ -1,4 +0,0 @@
-#include <javacall_misc.h>
-
-void javacall_misc_set_cpu_speed(int speed) {
-}
