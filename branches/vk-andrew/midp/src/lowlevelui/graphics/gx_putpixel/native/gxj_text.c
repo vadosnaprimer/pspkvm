68a82297354d319a06e6d40d5da6d9a08fed1e92
ajmilne <ajmilne@2af63af8-f945-0410-af61-887fe020137e>
2009-07-06 05:48:03 +0000

    Fix to lookup for characters in upper half of codepage 0 in the built-in font--restores common roman vowels with common diacritics incl. grave, acute, umlaut/diaeresis et al.

--- branches/vk-andrew/midp/src/lowlevelui/graphics/gx_putpixel/native/gxj_text.c
+++ branches/vk-andrew/midp/src/lowlevelui/graphics/gx_putpixel/native/gxj_text.c
@@ -82,6 +82,12 @@ static void drawChar(gxj_screen_buffer *sbuf, jchar c0,
     unsigned short CJK = ((short*)UNI_CJK)[c0] < 256 && ((short*)UNI_CJK)[c0] > 0?
 		                     ((short*)UNI_CJK)[c0]:
 	                         ((((short*)UNI_CJK)[c0] >> 8) & 0xff) | (((short*)UNI_CJK)[c0] << 8);
+	  // Whatever else is going on here, we can confirm that this operation DOES NOT WORK
+	  // for many characters in 0x0080 - 0x00ff -- and an easy fix is not to do it
+	  // for the entire Unicode page. Note also that it's probably bad news for the first
+	  // several pages ... I know these; they shouldn't need such a transform.
+	  if ((c0 & 0xff00) == 0) {
+			CJK = c0; }
     unsigned char const * fontbitmap =
         selectFontBitmap(CJK,pfonts) + FONT_DATA;
     jchar const c = (CJK & 0xff) -
