27d372d6fd4501870430714c2a424aba908242b9
ajmilne <ajmilne@2af63af8-f945-0410-af61-887fe020137e>
2009-10-15 00:10:50 +0000

    Display cleanups for Danzeff, semichordal boards

--- midp/src/highlevelui/keyboard/classes/javax/microedition/lcdui/KeyboardLayer_danzeff.java
+++ midp/src/highlevelui/keyboard/classes/javax/microedition/lcdui/KeyboardLayer_danzeff.java
@@ -225,7 +225,8 @@ class KeyboardLayer_danzeff extends AbstractKeyboardLayer implements CommandList
 				return; }
 			if (cvContext != null) {
 				cvContext.currentDisplay.requestScreenRepaint(); } }
-        
+
+    final static int EPAD = 2;
     /**
      * Sets the bounds of the popup layer.
      *
@@ -241,19 +242,19 @@ class KeyboardLayer_danzeff extends AbstractKeyboardLayer implements CommandList
     	int ah = getAvailableHeight();
     	switch(current_quad) {
 				case TextFieldLFImpl.QUAD_TOPLFT:
-					super.setBounds(aw-w, ah-h, w, h);
+					super.setBounds(aw-w-EPAD, ah-h-EPAD, w, h);
 					break;
 				case TextFieldLFImpl.QUAD_TOPRGT:
-					super.setBounds(0, ah-h, w, h);
+					super.setBounds(EPAD, ah-h-EPAD, w, h);
 					break;
 				case TextFieldLFImpl.QUAD_BOTLFT:
-					super.setBounds(aw-w, 0, w, h);
+					super.setBounds(aw-w-EPAD, EPAD, w, h);
 					break;
 				case TextFieldLFImpl.QUAD_BOTRGT:
-					super.setBounds(0, 0, w, h);
+					super.setBounds(EPAD, EPAD, w, h);
 					break;
 				default:
-					super.setBounds(aw-w, ah-h, w, h); }
+					super.setBounds(aw-w-EPAD, ah-h-EPAD, w, h); }
 			requestFullScreenRepaint(); }
 
     /**
