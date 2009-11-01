dbb5c58f32e34af770d8f87cbf9b601a83120fc8
ajmilne <ajmilne@2af63af8-f945-0410-af61-887fe020137e>
2009-11-01 01:21:20 +0000

    Cleaned script building built-in font--

--- trunk/midp/src/tool/fontgen/src/build_tables.sh
+++ trunk/midp/src/tool/fontgen/src/build_tables.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+# Script to write the
+# midp/src/lowlevelui/graphics/gx_putpixel/native/gxj_font_bitmap.c file
+# (containing the built-in font's bitmaps) directly from the contents of
+# ../fontdef_ex. Uses wrfont2 and write_mother_table.pl.
+
 TGT=build/gxj_font_bitmap.c
 
+# Build the tool
+cd wrfont2
+make
+cd ..
+
+# Build the tables
 rm -rf build
 mkdir build
 wrfont2/wrfont2.exe header > $TGT
@@ -10,9 +21,7 @@ for file in `ls ../fontdef_ex/page*`; do
 	echo >> $TGT
 	wrfont2/wrfont2.exe table < $file >> $TGT
 	done
-rm -rf build/tables.txt
-grep 'unsigned char' $TGT | sed 's/build\/page_..\.c:unsigned char //' | sed 's/unsigned char //' | sed 's/\[...\]//' | sed 's/\[....\]//' | sed 's/ = {//' >> build/tables.txt
 echo >> $TGT
-./write_mother_table.pl >> $TGT
+./write_mother_table.pl $TGT >> $TGT
 echo >> $TGT
 cp $TGT /pspkvm/midp/src/lowlevelui/graphics/gx_putpixel/native/
--- trunk/midp/src/tool/fontgen/src/write_mother_table.pl
+++ trunk/midp/src/tool/fontgen/src/write_mother_table.pl
@@ -1,24 +1,29 @@
 #!/usr/bin/perl
 
 use strict;
-use FileHandle;
 
-my $fh = new FileHandle('build/tables.txt');
-if (!defined($fh)) {
-	die "Couldn't find tables list\n"; }
+# Collect the table names
+my $fn = $ARGV[0];
+my @tlist = `grep 'unsigned char page' $fn`;
+my @f = ();
+foreach my $l (@tlist) {
+	if ($l =~ /(page_...._....)/) {
+		push (@f, $1); } }
 
+# Prep the default array elements
 my @p = ();
 for (my $i=0; $i<256; $i++) {
 	$p[$i] = '(pfontbitmap)0'; }
 
-my @f = <$fh>;
+# Arrange the table
 foreach my $l (@f) {
 	if ($l =~ /page_(..)/) {
 		my $hw = $1;
 		chomp $l;
 		my $i = hex($hw);
 		$p[$i] = $l; } }
-		
+
+# Write it
 print "\n\n";
 print "pfontbitmap FontBitmaps[0x100] = {\n\t";
 print join(",\n\t", @p);
